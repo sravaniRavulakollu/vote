16500aa4ffc6440378343b0cdf2da334bc9d2bda
sravaniRavulakollu <87975506+sravaniRavulakollu@users.noreply.github.com>
2021-10-14 12:06:51 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace vote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if(age<18)
+            {
+                Console.WriteLine("Not Allowed to vote");
+            }
+            else
+            {
+                Console.WriteLine("Allowed to vote");
+            }
+
+
+        }
+    }
+}
